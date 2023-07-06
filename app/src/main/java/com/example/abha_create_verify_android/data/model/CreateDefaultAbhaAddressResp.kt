85de4af559b371c6376c3c50661e038441806dd2
SanoferSameera <79590907+SanoferSameera@users.noreply.github.com>
2023-07-06 16:37:10 +0530

    GOK-167 | Add. ABHA Number and ABHA Address creation (#1)
    * Create abha page added.
    
    * Add. api service and its required model class
    
    * new 3page page design added.
    
    * 167 | Add. api for aadhaar otp generation and verification
    
    * 3 new page design added.
    
    * Abha creation page design finished
    
    * 167 | Add. api for mobile otp generation, verification and abha number creation
    
    * 167 | Add. aadhaar consent and validation on aadhaar number
    
    * 167 | Refactor. to adjust the content in the grid
    
    * 167 | Add. api for abha address creation
    
    * 167 | Add. api for abha address creation
    
    * 167 | Fix. scroll bar in consent and progress bar in abha address creation
    
    * 167 | Removed unused imports and fixed warnings
    
    * 167 | Refactor. abha address creation
    
    * 167 | Refactor. extracted abha suffix to the string constant
    
    * 167 | Refactor. consents
    
    * 167 | Downgraded the sdk and removed commented lines
    
    * 167 | Add. handling back button
    
    ---------
    
    Co-authored-by: sudiptaDuttaTW <sudipta.dutta@thoughtworks.com>

---
+++ app/src/main/java/com/example/abha_create_verify_android/data/model/CreateDefaultAbhaAddressResp.kt
@@ -0,0 +1,7 @@
+package com.example.abha_create_verify_android.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class CreateDefaultAbhaAddressResp(
+    @SerializedName("healthId") val abhaAddress: String,
+)
