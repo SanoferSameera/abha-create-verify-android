fc63e43ce5450e7f1c3cffa5fafe12ffc8f29b4e
SanoferSameera <sanofersameera25@gmail.com>
2023-07-10 11:14:29 +0530

    165 | To read and pass the session token to the api calls

--- app/src/main/java/com/example/abha_create_verify_android/data/api/RetrofitBuilder.kt
+++ app/src/main/java/com/example/abha_create_verify_android/data/api/RetrofitBuilder.kt
@@ -1,18 +1,39 @@
 package com.example.abha_create_verify_android.data.api
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import okhttp3.Interceptor
+import okhttp3.logging.HttpLoggingInterceptor
+import java.util.concurrent.TimeUnit
 
-object RetrofitBuilder {
+    object RetrofitBuilder {
 
-    private const val BASE_URL = ""
+        private const val BASE_URL = ""
+        private const val HEADER_AUTHORIZATION = "Authorization"
+        var AUTH_TOKEN = ""
 
-    private fun getRetrofit(): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build() //Doesn't require the adapter
+        private fun getRetrofit(): Retrofit {
+
+            val okHttpClient = OkHttpClient.Builder().apply {
+                addInterceptor(
+                    Interceptor { chain ->
+                        val builder = chain.request().newBuilder()
+                        builder.header(HEADER_AUTHORIZATION, "Bearer $AUTH_TOKEN")
+                        readTimeout(10, TimeUnit.SECONDS)
+                        connectTimeout(5, TimeUnit.SECONDS)
+                        return@Interceptor chain.proceed(builder.build())
+                    }
+                )
+
+            }.build()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+
+        val apiService: ApiService = getRetrofit().create(ApiService::class.java)
     }
-
-    val apiService: ApiService = getRetrofit().create(ApiService::class.java)
-}
