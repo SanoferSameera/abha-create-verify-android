39686e82cc321624889d1e235a7141ab3aa3ffaf
SanoferSameera <79590907+SanoferSameera@users.noreply.github.com>
2023-09-12 11:30:50 +0530

    Gok-346 | Fixed aadhaar demographics dob issue and disabled aadhaar demographics (#13)
    * 346 | Add. to choose birth date from DatePicker
    
    * 346 | Refactor.To show a unavailable toast notification on click of demo auth mode
    
    * 346 | Removed commented code

--- app/src/main/java/com/example/abha_create_verify_android/DemographicActivity.kt
+++ app/src/main/java/com/example/abha_create_verify_android/DemographicActivity.kt
@@ -1,14 +1,20 @@
 package com.example.abha_create_verify_android
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import com.example.abha_create_verify_android.databinding.ActivityDemographicBinding
 import com.example.abha_create_verify_android.utils.DialogUtils
+import com.google.android.material.textfield.TextInputEditText
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class DemographicActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDemographicBinding
+    private lateinit var dobEditText: TextInputEditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,11 +25,39 @@ class DemographicActivity : AppCompatActivity() {
         supportActionBar?.title = resources.getString(R.string.create_abha)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        binding.appBarLayout.includeToolbar.customCloseButton.setOnClickListener { v ->
+        dobEditText = findViewById(R.id.dobEditText)
+
+        dobEditText.setOnClickListener {
+            showDatePickerDialog()
+        }
+
+        binding.appBarLayout.includeToolbar.customCloseButton.setOnClickListener {
             DialogUtils.showConfirmationDialog(this) {
                 onBackPressedDispatcher.onBackPressed()
             }
         }
+    }
+
+    private fun showDatePickerDialog() {
+        val calendar = Calendar.getInstance()
+        val datePickerDialog = DatePickerDialog(
+            this,
+            { _, year, monthOfYear, dayOfMonth ->
+                val selectedDate = Calendar.getInstance()
+                selectedDate.set(year, monthOfYear, dayOfMonth)
+
+                val dateFormat = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
+                dobEditText.setText(dateFormat.format(selectedDate.time))
+            },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+
+         val maxDate = Calendar.getInstance()
+         datePickerDialog.datePicker.maxDate = maxDate.timeInMillis
+
+        datePickerDialog.show()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
