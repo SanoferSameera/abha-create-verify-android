d88eb0b7c3ae0161fcf94a4fb47c0526469a360b
SanoferSameera <79590907+SanoferSameera@users.noreply.github.com>
2023-11-20 15:45:11 +0530

    Gok-424 | Add. to decode secured Aadhaar qr code (#15)
    * 424 | Add.  decode secured Aadhaar qr code
    
    Co-authored-by: yenugukeerthana <keerthana.yenugu@thoughtworks.com>

---
+++ app/src/main/java/com/example/abha_create_verify_android/utils/AadhaarCardInfo.kt
@@ -0,0 +1,14 @@
+package com.example.abha_create_verify_android.utils
+
+data class AadhaarCardInfo (
+
+    var name : String = "",
+    var dateOfBirth : String = "",
+    var gender : String = "",
+    var villageTownCity : String = "",
+    var subDistrict : String  = "",
+    var district : String  = "",
+    var pinCode : String  = "",
+    var state : String  = "",
+    var last4DigitAadhaar : String  = "",
+)
