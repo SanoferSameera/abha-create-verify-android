9c45fb7b7423ccda768e54782ee29922937cc30e
SanoferSameera <sanofersameera25@gmail.com>
2023-11-22 21:26:53 +0530

    Add. Aadhaar QR code functionality

---
+++ abha-create-verify/src/main/java/com/example/abha_create_verify/DemographicsManualOrQRScanActivity.kt
@@ -0,0 +1,121 @@
+package com.example.abha_create_verify
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.example.abha_create_verify.databinding.ActivityDemographicsManualOrQrscanBinding
+import com.example.abha_create_verify.utils.DialogUtils
+import com.google.android.gms.common.moduleinstall.ModuleInstall
+import com.google.android.gms.common.moduleinstall.ModuleInstallRequest
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
+
+
+class DemographicsManualOrQRScanActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDemographicsManualOrQrscanBinding
+    private val successMessage = MutableLiveData<String>()
+    private val failureMessage = MutableLiveData<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDemographicsManualOrQrscanBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarLayout.includeToolbar.toolbarAbha)
+        supportActionBar?.title = resources.getString(R.string.create_abha)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.scanQRButton.setOnClickListener {
+            launchScanner()
+        }
+
+        binding.enterManuallyButton.setOnClickListener {
+            moveToManualDemographic()
+        }
+        binding.appBarLayout.includeToolbar.customCloseButton.setOnClickListener { v ->
+            DialogUtils.showConfirmationDialog(this) {
+                onBackPressedDispatcher.onBackPressed()
+            }
+        }
+
+        successMessage.observe(this, Observer { message ->
+            showMessage(message)
+        })
+
+        failureMessage.observe(this, Observer { message ->
+            showMessage(message)
+        })
+    }
+
+    private fun launchScanner(): String? {
+        val options = GmsBarcodeScannerOptions.Builder()
+            .setBarcodeFormats(
+                Barcode.FORMAT_QR_CODE,
+                Barcode.FORMAT_AZTEC
+            )
+            .build()
+        var data: String? = null;
+        val scanner = GmsBarcodeScanning.getClient(this, options)
+
+        val moduleInstallRequest =
+            ModuleInstallRequest.newBuilder()
+                .addApi(scanner)
+                .build()
+        val moduleInstallClient = ModuleInstall.getClient(this)
+
+        moduleInstallClient
+            .installModules(moduleInstallRequest)
+            .addOnSuccessListener {
+                Log.d("QR", "Modules successfully installed")
+            }
+            .addOnFailureListener {
+                Log.e("MainActivity", "Error installing modules", it)
+            }
+
+        scanner.startScan()
+            .addOnSuccessListener { barcode ->
+                data = barcode.rawValue;
+                Log.d("QR", "Successful: $data")
+                successMessage.postValue("Successfully done scanning: $data")
+            }
+            .addOnFailureListener { e ->
+                val errorMessage = "Some unexpected error occurred: $e"
+                Log.e("QR", "failed:  $e")
+                failureMessage.postValue(errorMessage)
+            }
+        return data;
+    }
+
+    private fun moveToManualDemographic() {
+        val aadhaarNumber = intent.getStringExtra("aadhaarNumber")
+        val intent = Intent(this, DemographicActivity::class.java)
+        intent.putExtra("aadhaarNumber", aadhaarNumber)
+        startActivity(intent)
+        finish()
+    }
+
+    override fun onBackPressed() {
+        val intent = Intent(this, AuthModeActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            onBackPressed()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+}
---
+++ abha-create-verify/src/main/java/com/example/abha_create_verify/utils/AadhaarSecureQrParser.kt
@@ -0,0 +1,122 @@
+package com.example.abha_create_verify.utils
+
+import android.util.Log
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.io.UnsupportedEncodingException
+import java.math.BigInteger
+import java.util.LinkedList
+import java.util.zip.GZIPInputStream
+
+
+class AadhaarSecureQrParser(scanData: String?) {
+    private var decodedData: ArrayList<String>? = null
+    private var scannedAadhaarCardInfo: AadhaarCardInfo = AadhaarCardInfo()
+
+    init {
+
+        val bigIntScanData = BigInteger(scanData.toString(), 10)
+        val byteScanData = bigIntScanData.toByteArray()
+        val decompressByteScanData = decompressData(byteScanData)
+
+        val parts = separateData(decompressByteScanData)
+        if (parts.isEmpty()) {
+            throw QrCodeException("Invalid QR Code Data, no parts found after splitting by delimiter")
+        }
+        decodeData(parts)
+
+        Log.d("Aadhaar QR", "Data Decoded")
+    }
+
+    @Throws(QrCodeException::class)
+    private fun decompressData(byteScanData: ByteArray): ByteArray {
+        val bos = ByteArrayOutputStream(byteScanData.size)
+        val bin = ByteArrayInputStream(byteScanData)
+        val gis: GZIPInputStream? = try {
+            GZIPInputStream(bin)
+        } catch (e: IOException) {
+            Log.e("Exception", "Decompressing QR code, Opening byte stream failed: $e")
+            throw QrCodeException("Error in opening Gzip byte stream while decompressing QR code", e)
+        }
+        var size = 0
+        val buf = ByteArray(1024)
+        while (size >= 0) {
+            try {
+                size = gis?.read(buf, 0, buf.size) ?: 0
+                if (size > 0) {
+                    bos.write(buf, 0, size)
+                }
+            } catch (e: IOException) {
+                Log.e(
+                    "Exception",
+                    "Decompressing QR code, writing byte stream failed: $e"
+                )
+                throw QrCodeException("Error in writing byte stream while decompressing QR code", e)
+            }
+        }
+        try {
+            gis?.close()
+            bin.close()
+        } catch (e: IOException) {
+            Log.e("Exception", "Decompressing QR code, closing byte stream failed: $e")
+            throw QrCodeException("Error in closing byte stream while decompressing QR code", e)
+        }
+        return bos.toByteArray()
+    }
+
+
+    private fun separateData(source: ByteArray): List<ByteArray> {
+        val separatorByte = 255.toByte()
+        val separatedParts: MutableList<ByteArray> = LinkedList()
+        var begin = 0
+        for (i in source.indices) {
+            if (source[i] == separatorByte) {
+                // skip if first or last byte is separator
+                if (i != 0 && i != (source.size - 1)) {
+                    separatedParts.add(source.copyOfRange(begin, i))
+                }
+                begin = i + 1
+            }
+        }
+        return separatedParts
+    }
+
+
+    @Throws(QrCodeException::class)
+    private fun decodeData(encodedData: List<ByteArray>) {
+        val i = encodedData.iterator()
+        decodedData = ArrayList()
+        while (i.hasNext()) {
+            try {
+                decodedData!!.add(String(i.next(), Charsets.ISO_8859_1))
+
+            } catch (e: UnsupportedEncodingException) {
+                Log.e("Exception", "Decoding QR code, ISO-8859-1 not supported: $e")
+                throw QrCodeException("Decoding QR code, ISO-8859-1 not supported", e)
+            }
+        }
+        var index=0
+        if(decodedData!![0] == "V2"){
+          index=1
+        }
+
+        scannedAadhaarCardInfo.name = decodedData!![2 + index]
+        scannedAadhaarCardInfo.dateOfBirth = decodedData!![3 + index]
+        scannedAadhaarCardInfo.gender = decodedData!![4 + index]
+        scannedAadhaarCardInfo.villageTownCity = decodedData!![15 + index]
+        scannedAadhaarCardInfo.subDistrict = decodedData!![14 + index]
+        scannedAadhaarCardInfo.district = decodedData!![6 + index]
+        scannedAadhaarCardInfo.pinCode = decodedData!![10 + index]
+        scannedAadhaarCardInfo.state = decodedData!![12 + index]
+    }
+
+    fun getScannedAadhaarInfo(): AadhaarCardInfo {
+        return scannedAadhaarCardInfo
+    }
+}
+
+internal class QrCodeException : Exception {
+    constructor(message: String?) : super(message)
+    constructor(message: String?, cause: Throwable?) : super(message, cause)
+}
