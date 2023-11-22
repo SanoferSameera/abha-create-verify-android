5a59df4446aa431e7ade17fa182d16e59b45cf4f
SanoferSameera <sanofersameera25@gmail.com>
2023-11-22 21:26:53 +0530

    Add. Aadhaar QR code functionality

---
+++ abha-create-verify/src/main/java/com/example/abha_create_verify/utils/AadhaarCardInfo.kt
@@ -0,0 +1,13 @@
+package com.example.abha_create_verify.utils
+
+data class AadhaarCardInfo (
+
+    var name : String? = null,
+    var dateOfBirth : String? = null,
+    var gender : String? = null,
+    var villageTownCity : String? = null,
+    var subDistrict : String? = null,
+    var district : String? = null,
+    var pinCode : String? = null,
+    var state : String? = null,
+)
