3d12ec0ffeb47f11fd0cc113e0aa4e8403703a9e
SanoferSameera <sanofersameera25@gmail.com>
2023-11-24 14:48:16 +0530

    Add. Scan Aadhar QR code for registering patients

--- app/src/main/java/com/example/abha_create_verify_android/data/api/RetrofitBuilder.kt
+++ app/src/main/java/com/example/abha_create_verify_android/data/api/RetrofitBuilder.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit
 
     object RetrofitBuilder {
 
-        var BASE_URL = ""
+        var BASE_URL = "https://api.dev.abhaya.care/"
         private const val HEADER_AUTHORIZATION = "Authorization"
         var AUTH_TOKEN = ""
 
